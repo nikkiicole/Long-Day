7eb4978597bb82be9bfb638f5e6bcb4780106a49
Nikki <user@Nikkis-Macbook.fios-router.home>
2021-03-08 14:24:36 -0500

    added appropriate files

---
+++ app.js
@@ -0,0 +1,9 @@
+async function getBreeds() {
+  try {
+    let response = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+    console.log(response.data);
+  }catch (err) {
+      console.log(err);
+    }
+}
+getBreeds()

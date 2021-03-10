e0b09e4825e4cef66570b6ca135b13197cf90db2
Nikki <user@Nikkis-Macbook.fios-router.home>
2021-03-10 12:23:44 -0500

    CLEANED UP JS FILE

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ async function getCocktails() {
       measure.setAttribute('id','Measure-ID')
       ingredient.setAttribute('id','Ingredient-ID')
     }
-
+//ALLOWS US TO PASS API INFO TO EACH RESPECTIVE FUNCTION ALLOWING US TO DISPLAY VALUES AND POST TO OUR HTML ELEMENTS
     displayCocktailName(response.data.drinks[0].strDrink)
     displayRecipeCardInstructions(response.data.drinks[0].strInstructions)
     displayCocktailImage(response.data.drinks[0].strDrinkThumb)
@@ -79,31 +79,25 @@ async function getCocktails() {
 }
 getCocktails()
 
+//CHANGES INNER TEXT  OF EACH RESPECTIVE ELEMENTS
 function displayCocktailName(cocktailName) {
-  // console.log(cocktailName)
   cocktailTitle.innerText = cocktailName
 }
 
 function displayRecipeCardInstructions(cocktailDirections) {
-  // console.log(cocktailDirections)
   cocktailInstructions.innerText =cocktailDirections
 }
 
 function  displayCocktailImage(cocktailImage) {
-  // console.log(cocktailImage)
   cocktailPicture.src = cocktailImage
 }
 
 
 cocktailButton.addEventListener("click", getCocktails)
 
-// CALLS FUNCTION TO RUN ABOVE SO DATA IS AVAILABLE 
-
-// this creates the call to the api and makes the data available for me to use while implementing a method to catch errors 
 async function getAdviceSlips() {
   try {
     let response = await axios.get("https://api.adviceslip.com/advice");
-    // console.log(response.data.slip.advice);
     //ALLOWS DISPLAYSLIP TO PULL FROM API AND PASS BELOW SO I CAN CONNECT THE HTML 
     displaySlip(response.data.slip.advice);
 
@@ -116,7 +110,6 @@ getAdviceSlips()
 
 //CREATES DISPLAY SLIP FUNCTION WE RUN ADVICE THROUGH WHICH IS THE PARAMETER  I SET TO USE WITHIN THIS FUNCTION 
 function displaySlip(advice) {
-  console.log(advice)
   adviceSlip.innerText = '"' + advice + '"'
 }
 
@@ -126,47 +119,3 @@ bothButton.addEventListener("click",() => {
 getAdviceSlips();
 getCocktails();
 });
-
-
-
-
-
-
-//Connect all API's 
-
-// connect random search data to give me a drink button
-// connect random advice data to give me advice button
-// link search by cocktail to search. html
-
-//create element for Cocktail name 
-//name element 
-// append cocktail name element to dom inside of black card 
-//connect to give me drink button 
-
-//create element for Cocktail ingredients
-//name element
-// append cocktail name element to dom inside of black card 
-//connect to give me drink button 
-
-//create element for Cocktail Recipe 
-//name element
-// append cocktail name element to dom inside of black card
-//connect to give me drink button 
-
-// append cocktail name element to dom inside of black card 
-//name element
-//create image element for cocktail photo 
-//connect to give me drink button 
-
-// remove sample card and replace with real recipe card when give me a drink is pressed
-// create function to remove each time the button is pressed 
-
-
-//create advice h1 when give me advice is pressed 
-//name and apphend to dom 
-// remove sample advice and replace with real advice card when give me advice is pressed
-// create function to remove old advice each time the button is pressed 
-
-
-
-
